054d4192ea8b41addc3433f296482ff019cbafcd
纪俊鹏 <jijunpeng1990@163.com>
2017-03-13 23:26:14 +0800

    test

---
+++ library/src/main/java/com/jijunpeng/javautils/BytesUtil.java
@@ -0,0 +1,10 @@
+package com.jijunpeng.javautils;
+
+/**
+ * Created by jijunpeng on 17/3/13.
+ */
+public class BytesUtil {
+    public static String getName(int num) {
+        return num + "str";
+    }
+}
